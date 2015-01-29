701c4b568d02e7d713d8cd7a0c5fb661f17b8b13
Benjamin Huskisson-Snider <snid0039@algonquinlive.com>
2015-01-29 17:49:23 -0500

    Update sign-in-project-db-creation.sql
    changed userIsAdmin field into string field instead of char field

--- sign-in-project-db-creation.sql
+++ sign-in-project-db-creation.sql
@@ -1,3 +1,3 @@
 CREATE database gaming;
 USE gaming;
-CREATE TABLE gaming.users (username VARCHAR(50) PRIMARY KEY, userpass VARCHAR(50) NOT NULL, userFirstName VARCHAR(100) NOT NULL, userLastName VARCHAR(100) NOT NULL, userAddress VARCHAR(500) NOT NULL, userCountry VARCHAR(100) NOT NULL, userCity VARCHAR(100) NOT NULL, userPostalCode VARCHAR(20) NOT NULL, userEmail VARCHAR(100) NOT NULL, userPhone VARCHAR(50) NOT NULL, userIsAdmin CHAR(1) DEFAULT 'n' NOT NULL);
+CREATE TABLE gaming.users (username VARCHAR(50) PRIMARY KEY, userpass VARCHAR(50) NOT NULL, userFirstName VARCHAR(100) NOT NULL, userLastName VARCHAR(100) NOT NULL, userAddress VARCHAR(500) NOT NULL, userCountry VARCHAR(100) NOT NULL, userCity VARCHAR(100) NOT NULL, userPostalCode VARCHAR(20) NOT NULL, userEmail VARCHAR(100) NOT NULL, userPhone VARCHAR(50) NOT NULL, userIsAdmin VARCHAR(3) DEFAULT 'no' NOT NULL);
